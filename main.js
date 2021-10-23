3014f8d0ad372d554796bced3106ab1b5f078106
TechPurbayan <93037799+TechPurbayan@users.noreply.github.com>
2021-10-23 17:22:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function clearscreen()
+{
+    document.getElementById("result").value="";
+}
+function display(value)
+{
+    document.getElementById("result").value += value;
+}
+function calculate()
+{
+    var n1=document.getElementById("result").value;
+    var n2= eval(n1);
+    document.getElementById("result").value=n2;
+}
